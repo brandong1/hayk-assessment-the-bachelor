cfd66c9860b3e3802d3ef052c94620ab7b0804e0
github username <github email address>
2020-02-21 08:51:44 -0700

    Add local project to GitHub

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,24 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant_hash|
+    if contestant_hash["status"] == "Winner"
+      return contestant_hash["name"].split(" ").first
+    end
+  end
 end
+  # data.each do |season_name, contestant_data|
+  #   contestant_data.each do |name|
+  #     return contestant_data[:name] if name[:state] == "Winner"  
+  #   end
+  #   binding.pry
+   
+
+  # data.each do |season, winner|
+  #   if season == "season 10"
+  #     winner[:status].find |season_winner|
+  #       season_winner[:name].split if season_winner[:status] == "Winner"
+  #     end
+  #   end
 
 def get_contestant_name(data, occupation)
   # code here
